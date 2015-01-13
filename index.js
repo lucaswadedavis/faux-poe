388eb93d767ff9e5592a72e71d85cec3c958116c
Luke Davis <lukefable@gmail.com>
2015-01-13 07:18:45 +0000

    moving along: pulled out couplet and combine lines functions - later make sure the combine lines thing will allow for odd-numbered inputs.

--- index.js
+++ index.js
@@ -25,11 +25,33 @@ var fauxPoe=function(seed){
     "wandering",
     "forelorn"
     ];
+  var pPhrase=function(){
+    var phrases=[
+      "in the valley of the",
+      "in the",
+      "upon the",
+      "on the",
+      "with the",
+      "by the",
+      "beside the",
+      "within the",
+      "beneath the"
+      ]
+    return c.pick(phrases)+" "+c.pick(nouns);
+  };
   var d="";
-  d+="In the valley of the "+c.pick(nouns)+", "+c.pick(adjectives)+"\n";
-  d+="A "+c.pick(adjectives)+" silence in the first epoch.";
+  
+  var couplet=function(){
+    var d=[];
+    d.push(pPhrase()+" was a "+c.pick(nouns)+", "+c.pick(adjectives));
+    d.push(c.first({gender:'female'})+" "+c.pick(adjectives)+" "+c.pick(adjectives)+".");
+    return d;
+  };
+  var combineLines=function(l1,l2){
+    return l1[0]+"\n"+l2[0]+"\n"+l1[1]+"\n"+l2[1];
+  };
+  d+=combineLines(couplet(), couplet() );
   return d;
 };
 
 console.log( fauxPoe() );
-console.log((new Chance() ).first({gender:''}) );
