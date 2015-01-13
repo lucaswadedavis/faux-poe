9c38e7953238bb36f6d5748f58431b441430a782
Luke Davis <lukefable@gmail.com>
2015-01-13 06:52:33 +0000

    updated dependencies to include chance.

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-fauxPoe=function(){
+var Chance=require("chance");
+
+var fauxPoe=function(seed){
+  var c = seed ? new Chance(seed) : new Chance();
+  var nouns=[
+    "maiden",
+    "mountain",
+    "river",
+    "shore",
+    "wall",
+    "heath",
+    "moore",
+    "wood",
+    "manse",
+    "fever",
+    "heart",
+    "love",
+    "night",
+    "bosom",
+    "star"
+    ];
+  var adjectives=[
+    "broken",
+    "torn",
+    "wandering",
+    "forelorn"
+    ];
   var d="";
-  d+="so poe";
+  d+="In the valley of the "+c.pick(nouns)+", "+c.pick(adjectives)+"\n";
+  d+="A "+c.pick(adjectives)+" silence in the first epoch.";
   return d;
 };
 
 console.log( fauxPoe() );
+console.log((new Chance() ).first({gender:''}) );
